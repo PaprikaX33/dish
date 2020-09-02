7cba7008a66a1cf196f9b7399adab42393b4464f
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-09-02 09:17:02 +0700

    cleaned up commented out code

--- src/global.c
+++ src/global.c
@@ -2,14 +2,6 @@
 #include <unistd.h>
 
 int initialize_global(void){
-  //STDOUT_FILENO, STDERR_FILENO
-  //global.outTty, global.errTty
-  /* if(isatty(STDIN_FILENO)){ */
-  /*   global.inTty = 1; */
-  /* } */
-  /* else{ */
-  /*   global.inTty = 0; */
-  /* } */
   global.inTty = isatty(STDIN_FILENO);
   global.outTty = isatty(STDOUT_FILENO);
   global.errTty = isatty(STDERR_FILENO);
