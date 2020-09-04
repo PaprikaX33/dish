525b4f3b3a1464d4e0df4f93c01bf3a897eed019
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-09-04 07:07:43 +0700

    completed the string token but infinite loop

--- src/token.c
+++ src/token.c
@@ -1,4 +1,5 @@
 #include "Tokenizer.h"
+#include "Memory.h"
 #include <stddef.h>
 #include <string.h>
 #include <ctype.h>
@@ -23,25 +24,54 @@ char const * dish_tokenize(char const * str, struct TokenNode * tok)
     str++;
   }
 #endif
+  /* //////////////////// */
   if(*str == '\0'){
     tok->type = TOK_END;
     return str+1;
   }
+  int inApos = 0;
   char * strBuff = 0;
   size_t strPos = 0;
-  while(!isspace(str[strPos]) && str[strPos] != '\0'){
-    strPos++;
-  }
-  strBuff = malloc((sizeof(char) * strPos) + 1u);
-  if(!strBuff){
-    fprintf(stderr, "Unable to allocate memory!");
-    exit(-1);
-  }
-  memcpy(strBuff, str, strPos);
-  strBuff[strPos] = '\0';
-  tok->type = TOK_STRING;
+  int repeat = 0;
+  do{
+    repeat = 0;
+    while(!isspace(str[strPos]) &&
+          str[strPos] != '\'' &&
+          str[strPos] != '\"' &&
+          str[strPos] != '|' &&
+          str[strPos] != '>' &&
+          str[strPos] != '<' &&
+          str[strPos] != '\0'){
+      strPos++;
+    }
+    strBuff = xrealloc(strBuff, (sizeof(char) * strPos) + 1u, "Unable to allocate memory!");
+    memcpy(strBuff, str, strPos);
+    strBuff[strPos] = '\0';
+    tok->type = TOK_STRING;
+
+    if(str[strPos] == '\'' || str[strPos] == '\"'){
+      int delim = str[strPos];
+      size_t finder = 1;
+      while(str[strPos + finder] != delim && str[strPos + finder] != '\0'){
+        finder++;
+      }
+      if(str[strPos + finder] == '\0'){
+        /* Unbalanced apostroph */
+        return NULL;
+      }
+      strBuff = xrealloc(strBuff, (sizeof(char) * (strPos + finder)) + 1u, "Unable to allocate memory!");
+      memcpy(strBuff + strPos, str + strPos, finder);
+      strBuff[strPos + finder] = '\0';
+      /* This part is infinite looping */
+      if(!isspace(str[strPos + finder + 1u]) && strBuff[strPos + finder + 1] != '\0'){
+        repeat = 1;
+      }
+    }
+  }while(repeat);
+
   tok->str = strBuff;
   return str + strPos;
+  /* //////////////////// */
 
 #if 0
   /*Check for a quoted string*/
