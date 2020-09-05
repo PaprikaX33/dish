76599ee1702682970a467bcaced39dd4f8ab6003
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-09-06 03:28:14 +0700

    removed the repeat mechanism. Need a better approach

--- src/token.c
+++ src/token.c
@@ -20,12 +20,6 @@ char const * dish_tokenize(char const * str, struct TokenNode * tok)
   while(isspace(*str)){
     str++;
   }
-#if 0
-  /* A stop plug to test the end token */
-  while(*str != '\0'){
-    str++;
-  }
-#endif
   /* //////////////////// */
   if(*str == '\0'){
     tok->type = TOK_END;
@@ -34,70 +28,40 @@ char const * dish_tokenize(char const * str, struct TokenNode * tok)
   int inApos = 0;
   char * strBuff = 0;
   size_t strPos = 0;
-  int repeat = 0;
-  do{
-    repeat = 0;
-    while(!is_special(str[strPos])){
-      strPos++;
-    }
-    strBuff = xrealloc(strBuff, (sizeof(char) * strPos) + 1u, "Unable to allocate memory!");
-    memcpy(strBuff, str, strPos);
-    strBuff[strPos] = '\0';
-    tok->type = TOK_STRING;
+  while(!is_special(str[strPos])){
+    strPos++;
+  }
+  strBuff = xrealloc(strBuff, (sizeof(char) * strPos) + 1u, "Unable to allocate memory!");
+  memcpy(strBuff, str, strPos);
+  strBuff[strPos] = '\0';
+  tok->type = TOK_STRING;
 
-    if(str[strPos] == '\'' || str[strPos] == '\"'){
-      int delim = str[strPos];
-      size_t finder = 1;
-      while(str[strPos + finder] != delim && str[strPos + finder] != '\0'){
-        finder++;
-      }
-      if(str[strPos + finder] == '\0'){
-        /* Unbalanced apostroph */
-        return NULL;
-      }
-      printf("Found between %lu and %lu\n", strPos, finder);
-      strBuff = xrealloc(strBuff, (sizeof(char) * (strPos + finder)) + 1u, "Unable to allocate memory!");
-      /* Apparently this works but seems wrong tho */
-      memcpy(strBuff + strPos, str + strPos + 1u, finder - 1u);
-      strBuff[strPos + finder] = '\0';
-      /* This part is infinite looping */
-      /* if(!isspace(str[strPos + finder + 1u]) && strBuff[strPos + finder + 1u] != '\0'){ */
-      /*   repeat = 1; */
-      /* } */
-      tok->str = strBuff;
-      return str + strPos + finder + 1u;
+  if(str[strPos] == '\'' || str[strPos] == '\"'){
+    int delim = str[strPos];
+    size_t finder = 1;
+    while(str[strPos + finder] != delim && str[strPos + finder] != '\0'){
+      finder++;
     }
-  }while(repeat);
+    if(str[strPos + finder] == '\0'){
+      /* Unbalanced apostroph */
+      return NULL;
+    }
+    printf("Found between %lu and %lu\n", strPos, finder);
+    strBuff = xrealloc(strBuff, (sizeof(char) * (strPos + finder)) + 1u, "Unable to allocate memory!");
+    /* Apparently this works but seems wrong tho */
+    memcpy(strBuff + strPos, str + strPos + 1u, finder - 1u);
+    strBuff[strPos + finder] = '\0';
+    /* This part is infinite looping */
+    /* if(!isspace(str[strPos + finder + 1u]) && strBuff[strPos + finder + 1u] != '\0'){ */
+    /*   repeat = 1; */
+    /* } */
+    tok->str = strBuff;
+    return str + strPos + finder + 1u;
+  }
 
   tok->str = strBuff;
   return str + strPos;
   /* //////////////////// */
-
-#if 0
-  /*Check for a quoted string*/
-  if(*str == '\"'){
-    str++;
-    /* Search for the matching pair of quoted string */
-    while(str[endPos] != '\"' && str[endPos]){
-      endPos++;
-    }
-    /* Check if the quote is unbalanced */
-    if(!str[endPos]){
-      return NULL;
-    }
-    char * strBuff = malloc(endPos * sizeof(char));
-    if(!strBuff){
-      fprintf(stderr, "Unable to allocate memory!");
-      exit(-1);
-    }
-    memcpy(strBuff, str, endPos);
-    strBuff[endPos - 1] = '\0';
-    tok->type = TOK_STRING;
-    tok->str = strBuff;
-    return str + endPos + 1u;
-  }
-  /* Not in quoted string */
-#endif /* if 0 */
   return str + endPos;
 }
 
