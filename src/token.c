029cb78e66a516ef20aa5ce91a7f86ceef737a2f
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-09-06 09:53:33 +0700

    tokenizing completed

--- src/token.c
+++ src/token.c
@@ -71,25 +71,37 @@ char const * process_str(char const * str, struct TokenNode * tok)
 {
   char * strBuff = NULL;
   size_t curSize = 0;
-  char const * retStr = string_token(str, &strBuff, &curSize);
+  int repeat = 0;
+  char const * retStr = str;
+  do{
+    repeat = 0;
+    retStr = string_token(retStr, &strBuff, &curSize);
+    if(!retStr){
+      return retStr;
+    }
+    if(retStr[0] == '\'' || retStr[0] == '"'){
+      /* Found a quoter */
+      int delim = retStr[0];
+      char const * findStr = retStr + 1u;
+      size_t finder = 0;
+      repeat = 1;
+      while(findStr[finder] != delim && findStr[finder] != '\0'){
+        finder++;
+      }
+      if(findStr[finder] == '\0'){
+        return NULL;
+      }
+      strBuff = xrealloc(strBuff, (sizeof(char) * (finder + curSize)) + 1u, "Unable to allocate memory!");
+      char * strBuffMod = strBuff + curSize;
+      memcpy(strBuffMod, findStr, finder);
+      strBuffMod[finder] = '\0';
+      curSize += finder;
+      retStr = findStr + finder + 1u;
+    }
+  }while(repeat);
   tok->type = TOK_STRING;
   tok->str = strBuff;
   return retStr;
-#if 0
-  size_t endPos = 0;
-  int inApos = 0;
-  char * strBuff = 0;
-  size_t strPos = 0;
-  while(!is_special(str[strPos])){
-    strPos++;
-  }
-  strBuff = xrealloc(strBuff, (sizeof(char) * strPos) + 1u, "Unable to allocate memory!");
-  memcpy(strBuff, str, strPos);
-  strBuff[strPos] = '\0';
-  tok->type = TOK_STRING;
-  tok->str = strBuff;
-#endif /* if 0 */
-
 #if 0
   if(str[strPos] == '\'' || str[strPos] == '\"'){
     int delim = str[strPos];
@@ -121,6 +133,9 @@ char const * string_token(char const * str, char ** curStr, size_t * curSize)
   size_t strPos = 0;
   while(!is_special(str[strPos])){
     strPos++;
+  }
+  if(!strPos){
+    return str;
   }
   size_t const strFinSize = strPos + *curSize;
   char * strBuff = xrealloc(*curStr, (sizeof(char) * (strFinSize)) + 1u, "Unable to allocate memory!");
