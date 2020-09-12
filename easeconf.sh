3c0b4d0230352b2ffa187ddc6408088709f288c8
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-09-12 18:35:51 +0700

    added the CXXFLAGS to the  easyconfig script

--- easeconf.sh
+++ easeconf.sh
@@ -9,6 +9,6 @@ then
 fi
 
 mkdir -p "$target/build" "$target/install/share"
-echo -e 'CPPFLAGS=-DDEBUG\nCFLAGS="${CFLAGS} -std=c11 -Wall -Wextra -Wshadow -Wcast-align -Wunused -Wpedantic -Wconversion -Wsign-conversion -Wnull-dereference -Wdouble-promotion -Wformat=2"' > "$target/install/share/config.site"
+echo -e 'CPPFLAGS=-DDEBUG\nCFLAGS="${CFLAGS} -std=c11 -Wall -Wextra -Wshadow -Wcast-align -Wunused -Wpedantic -Wconversion -Wsign-conversion -Wnull-dereference -Wdouble-promotion -Wformat=2"\nCXXFLAGS="${CXXFLAGS} -g -Wall -Wextra -Werror -Og -Wshadow -Wnon-virtual-dtor -Wold-style-cast -Wcast-align -Wunused -Woverloaded-virtual -Wpedantic -Wconversion -Wsign-conversion -Wnull-dereference -Wdouble-promotion -Wformat=2 -Wduplicated-cond -Wduplicated-branches -Wlogical-op -Wuseless-cast"' > "$target/install/share/config.site"
 cd "$target/build"
 $configScript --prefix="$target/install"
