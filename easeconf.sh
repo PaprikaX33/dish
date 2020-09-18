e267cb2b44684a864414e6db0506288aec9e3e60
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-09-19 02:32:49 +0700

    added the check for an exisiting config.site file

--- easeconf.sh
+++ easeconf.sh
@@ -2,18 +2,24 @@
 
 target="/tmp/dll"
 configScript="$PWD/configure"
+configSiteFile="$target/install/share/config.site"
+buildDir="$target/dish"
 
 if [ "$1" ]
 then
     target="$1"
 fi
 
-mkdir -p "$target/build" "$target/install/share"
-cat > "$target/install/share/config.site" <<EOF
+mkdir -p "$buildDir" "$target/install/share"
+
+cd "$buildDir"
+
+if [ ! -s $configSiteFile ]; then
+    cat > "$configSiteFile" <<EOF
 CPPFLAGS=-DDEBUG
 CFLAGS="\${CFLAGS} -std=c11 -Wall -Wextra -Wshadow -Wcast-align -Wunused -Wpedantic -Wconversion -Wsign-conversion -Wnull-dereference -Wdouble-promotion -Wformat=2"
 CXXFLAGS="\${CXXFLAGS} -g -Wall -Wextra -Werror -Og -Wshadow -Wnon-virtual-dtor -Wold-style-cast -Wcast-align -Wunused -Woverloaded-virtual -Wpedantic -Wconversion -Wsign-conversion -Wnull-dereference -Wdouble-promotion -Wformat=2 -Wduplicated-cond -Wduplicated-branches -Wlogical-op -Wuseless-cast"
 EOF
+fi
 
-cd "$target/build"
 $configScript --prefix="$target/install"
