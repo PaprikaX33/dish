5c6edecba2a1c17eebcc9a74b78b75361b6d025b
PaprikaX33 <paprika.trisalted.pepper@protonmail.com>
2020-09-20 22:51:51 +0700

    can extract the command token

--- rec/commandSpr.cpp
+++ rec/commandSpr.cpp
@@ -61,6 +61,11 @@ Di::CommandArr Di::parse_token(Di::TokenArr const & tok)
       throw Di::Exc::UnimplementedToken{};
     }
     // First string should be the command
+    if(curTok->_type != Di::TokenType::TOK_STRING &&
+       curTok->_type != Di::TokenType::TOK_VAR){
+      throw Di::Exc::SyntaxError{curTok->_type};
+    }
+    block._cmd = *curTok;
   }while(!stopParse || curTok != endTok);
   return arr;
 }
